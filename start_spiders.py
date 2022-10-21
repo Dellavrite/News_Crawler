567474ee31291e518dd78122d1c00f5ca630447e
Dellavrite <delavrite@gmail.com>
2022-10-22 01:31:47 +0300

    init

--- start_spiders.py
+++ start_spiders.py
@@ -2,7 +2,7 @@ import subprocess
 
 
 def main():
-    subprocess.call(["cd", "news_scraper/spiders/"])
+    subprocess.call("cd news_scraper/spiders/", shell=True)
     subprocess.call(["scrapy", "crawl", "hi-tech_crawl", "-o", "../../../data/hi-tech-scraped_data.csv"])
     subprocess.call(["scrapy", "runspider", "habr_news.py", "-o", "../../../data/habr-scraped_data.csv"])
 
