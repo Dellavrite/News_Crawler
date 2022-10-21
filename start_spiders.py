8c761da4309117e8e700c872d3182af423be0b03
Dellavrite <delavrite@gmail.com>
2022-10-22 01:35:55 +0300

    init

--- start_spiders.py
+++ start_spiders.py
@@ -3,8 +3,8 @@ import subprocess
 
 def main():
     subprocess.call("cd news_scraper/spiders/", shell=True)
-    subprocess.call(["scrapy", "crawl", "hi-tech_crawl", "-o", "../../../data/hi-tech-scraped_data.csv"])
-    subprocess.call(["scrapy", "runspider", "habr_news.py", "-o", "../../../data/habr-scraped_data.csv"])
+    subprocess.Popen(["scrapy", "crawl", "hi-tech_crawl", "-o", "../../../data/hi-tech-scraped_data.csv"])
+    subprocess.Popen(["scrapy", "runspider", "habr_news.py", "-o", "../../../data/habr-scraped_data.csv"])
 
 
 if __name__ == "__main__":
