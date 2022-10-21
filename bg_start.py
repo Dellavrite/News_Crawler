306e2909de3497fac5a96ddc2ae0b72f1af1ebdf
Dellavrite <delavrite@gmail.com>
2022-10-22 02:01:52 +0300

    init

---
+++ bg_start.py
@@ -0,0 +1,2 @@
+import subprocess
+subprocess.Popen(["python3", "./news_scraper/spiders/start_spiders.py"])
--- news_scraper/spiders/start_spiders.py
+++ news_scraper/spiders/start_spiders.py
@@ -2,8 +2,8 @@ import subprocess
 
 
 def main():
-    subprocess.Popen(["scrapy", "crawl", "hi-tech_crawl", "-o", "../../../data/hi-tech-scraped_data.csv"])
-    subprocess.Popen(["scrapy", "runspider", "habr_news.py", "-o", "../../../data/habr-scraped_data.csv"])
+    subprocess.run(["scrapy", "crawl", "hi-tech_crawl", "-o", "../../../data/hi-tech-scraped_data.csv"])
+    subprocess.run(["scrapy", "runspider", "habr_news.py", "-o", "../../../data/habr-scraped_data.csv"])
 
 
 if __name__ == "__main__":
