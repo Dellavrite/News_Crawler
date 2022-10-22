77af630b4dfe820a69b6e332c252da172a8630e3
Dellavrite <delavrite@gmail.com>
2022-10-22 19:41:26 +0300

    add starter

--- starters/start_habr_news.sh
+++ starters/start_habr_news.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-su - controller
-cd Project/News_Crawler/news_scraper/spiders
 scrapy runspider habr_news.py -o ../../../data/habr-scraped_data.csv
--- starters/start_hi_tech_crawl.sh
+++ starters/start_hi_tech_crawl.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-su - controller
-cd Project/News_Crawler/news_scraper/spiders
 scrapy crawl hi-tech_crawl -o ../../../data/hi-tech-scraped_data.csv
